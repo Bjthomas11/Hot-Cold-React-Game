6eb78534ed9808d709801a4d1dc955eaeef9ed89
Brian Thomas <Brianthomas571@gmail.com>
2018-12-30 22:44:03 -0600

    adding index.js info

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import MainGame from "./components/main-game";
+
+ReactDOM.render(<MainGame />, document.querySelector("#root"));
